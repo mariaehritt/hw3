d521b4f3d06c15b2c4ce20e20d9324da5848aab5
mariaehritt <maria.ehritt@kellogg.northwestern.edu>
2022-02-20 22:23:16 +0000

    Fleshing out posts and places

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
     def create
         @post = Post.new(params["post"])
         @post.save
-        redirect_to "/posts"
+        redirect_to "/places"
     end
     
 end
